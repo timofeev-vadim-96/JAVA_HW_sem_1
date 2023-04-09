ac4ef99030727d388c539661e769511e7ef12d63
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-04-09 03:30:55 +0300

    task4

--- src/sem1/HomeWorkSem1/HomeWorkSem1.java
+++ src/sem1/HomeWorkSem1/HomeWorkSem1.java
@@ -2,15 +2,17 @@ package sem1.HomeWorkSem1;
 import sem1.HomeWorkSem1.tasks.Task1;
 import sem1.HomeWorkSem1.tasks.Task2;
 import sem1.HomeWorkSem1.tasks.Task3;
+import sem1.HomeWorkSem1.tasks.Task4;
 
 public class HomeWorkSem1 {
     public static void main(String[] args) {
-        int inputNumb = Task1.inputNumb();
-        System.out.println("Треугльное число от 1 до " + inputNumb + " = " +
-                Task1.triangle(inputNumb)); //вычисление треугольного числа
-        System.out.println("Факториал числа "+ inputNumb + " = " +
-                Task2.factorial(inputNumb)); //факториал числа n
-        System.out.println("Простые числа от 1 до " + inputNumb + " :\n" +
-                Task3.simpleNumbers(inputNumb)); //простые числа
+//        int inputNumb = Task1.inputNumb();
+//        System.out.println("Треугльное число от 1 до " + inputNumb + " = " +
+//                Task1.triangle(inputNumb)); //вычисление треугольного числа
+//        System.out.println("Факториал числа "+ inputNumb + " = " +
+//                Task2.factorial(inputNumb)); //факториал числа n
+//        System.out.println("Простые числа от 1 до " + inputNumb + " :\n" +
+//                Task3.simpleNumbers(inputNumb)); //простые числа
+        System.out.println(Task4.SimpleCalc());
         }
     }
--- src/sem1/HomeWorkSem1/tasks/Task4.java
+++ src/sem1/HomeWorkSem1/tasks/Task4.java
@@ -1,4 +1,58 @@
+//Реализовать простой калькулятор
 package sem1.HomeWorkSem1.tasks;
 
+import java.util.Scanner;
+
 public class Task4 {
+    public static int inputNumb() {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        return number;
+    }
+    public static int menu() {
+        System.out.println("Выберите действие:\n" +
+                "1. Сумма\n" +
+                "2. Вычитание\n" +
+                "3. Умножение\n" +
+                "4. Деление\n" +
+                "5. Равно\n" +
+                "6. Выход");
+        int choice = inputNumb();
+        return choice;
+    }
+    public static double SimpleCalc() {
+        System.out.println("***Калькулятор***");
+        System.out.println("Введите число: ");
+        double result = inputNumb();
+        int inputChoice = menu();
+        while (true) {
+            if (inputChoice == 1) {
+                System.out.println("Введите число: ");
+                result += inputNumb();
+                inputChoice = menu();
+            }
+            if (inputChoice == 2) {
+                System.out.println("Введите число: ");
+                result -= inputNumb();
+                inputChoice = menu();
+            }
+            if (inputChoice == 3) {
+                System.out.println("Введите число: ");
+                result *= inputNumb();
+                inputChoice = menu();
+            }
+            if (inputChoice == 4) {
+                System.out.println("Введите число: ");
+                result /= inputNumb();
+                inputChoice = menu();
+            }
+            if (inputChoice > 6 || inputChoice < 1) {
+                System.out.println("Неверное действие, повторите попытку: ");
+                inputChoice = menu();
+            }
+            if (inputChoice == 5 || inputChoice==6){
+                return result;
+            }
+        }
+    }
 }
